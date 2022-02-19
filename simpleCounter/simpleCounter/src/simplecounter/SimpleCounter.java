db534f421b71bc16232ab4b7800d728479052360
HUSSAIN ASHIQ <74835399+hussainashiqktk@users.noreply.github.com>
2022-02-19 10:05:42 +0500

    Add files via upload

---
+++ simpleCounter/simpleCounter/src/simplecounter/SimpleCounter.java
@@ -0,0 +1,17 @@
+
+package simplecounter;
+
+/**
+ *
+ * @author HUSSAIN ASHIQ KHATTAK
+ */
+public class SimpleCounter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
+    }
+    
+}
