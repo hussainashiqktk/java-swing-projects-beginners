f69b3f728fe63da79df760064c33a047181bbb62
HUSSAIN ASHIQ <74835399+hussainashiqktk@users.noreply.github.com>
2022-02-22 11:53:08 +0500

    Add files via upload

---
+++ CharacterToASCII/src/charactortoascii/Methods.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package charactortoascii;
+
+/**
+ *
+ * @author greenhat
+ */
+public class Methods {
+    public static String getASCII(Character c){
+        int val = (int)c;
+        return String.valueOf(val);
+    }
+}
