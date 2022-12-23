9851e2c5a254d46d016cba7faf73bf65dffe0b65
HUSSAIN ASHIQ <74835399+hussainashiqktk@users.noreply.github.com>
2022-12-23 17:29:55 +0500

    Add files via upload

---
+++ SystemPathViewer/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(System.getenv("PATH"));
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+    }
+
+}
